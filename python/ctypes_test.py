f2404257bacd149dbfa3ec4ab45c2824efe56b35
Jani Hakala <jjhakala@gmail.com>
2016-07-27 11:08:20 +0300

    Add decode polyline functions

--- python/ctypes_test.py
+++ python/ctypes_test.py
@@ -16,13 +16,80 @@ def encode_coordinates(seq):
     arr = (_Coord * len(seq))()
 
     for i, (lat, long_) in enumerate(seq):
-	arr[i].latitude = lat
-	arr[i].longitude = long_
+        arr[i].latitude = lat
+        arr[i].longitude = long_
 
     buf = create_string_buffer(256)
     res = fun(buf, 256, arr, len(seq))
     return buf.value
 
-seq = [[1.0, 2.0], [3.0, 4.0]]
+def decode_polyline(pline, count=5):
+    fun = lib.decode_polyline_ffi
+    fun.argtypes = (c_void_p, POINTER(c_size_t), c_char_p)
+    fun.restype = c_int
+
+    coords = (_Coord * count)()
+    count = c_size_t(count)
+    res = fun(coords, byref(count), pline)
+    # print('Res: {:d} {:d}'.format(res, count.value))
+    if res < 0:
+        # call failed due to insufficient array size, required size also returned
+        coords = (_Coord * count.value)()
+        count = c_size_t(count.value)
+        res = fun(coords, byref(count), pline)
+        # print('Res: {:d} {:d}'.format(res, count.value))
+
+    seq = []
+    for i in range(count.value):
+        seq.append((coords[i].latitude, coords[i].longitude))
+
+    return seq
+
+class _FFIArray(Structure):
+    _fields_ = [("data", c_void_p), ("len", c_size_t)]
+
+class Coordinate(Structure):
+    _fields_ = [("latitude", c_double), ("longitude", c_double)]
+
+class Coordinates(Structure):
+    _fields_ = [("data", POINTER(Coordinate)), ("len", c_size_t)]
+
+import numpy as np
+from numpy.ctypeslib import ndpointer
+
+def decode_polyline2(pline, count=5):
+    fun = lib.decode_polyline_ffi2
+    fun.argtypes = [c_char_p]
+    fun.restype = _FFIArray
+
+    res = fun(pline)
+    cs = Coordinates(cast(res.data, POINTER(Coordinate)), res.len)
+
+    for i in range(cs.len):
+        print(cs.data[i].latitude, cs.data[i].longitude)
+
+    xs = np.ctypeslib.as_array((c_double * res.len * 2).from_address(res.data))
+    xs = xs.reshape((res.len, 2))
+    print(xs)
+
+    lib.free_coordinates(res.data)
+
+import random as r
+
+seq = []
+a = 60.0
+b = 150.0
+
+for i in range(10):
+    seq.append((a, b))
+    a += r.uniform(-0.5, 0.5)
+    b += r.uniform(-10.0, 10.0)
+
+import math
 res = encode_coordinates(seq)
-print(res)
+print(res, len(res))
+seq2 = decode_polyline(res, int(math.ceil(len(res) / 10.0)))
+print(seq, seq2)
+print('All ok? ', all(abs(x[0] - y[0]) < 0.00001 and abs(x[1] - y[1]) < 0.00001 for x, y in zip(seq, seq2)))
+
+decode_polyline2(res)
--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@ extern crate libc;
 extern crate polyline;
 
 use libc::*;
-use std::slice;
+use std::{ptr, slice};
 use std::ffi::*;
 
 use polyline::*;
@@ -13,10 +13,22 @@ pub extern "C" fn free_cstring(p: *mut c_char) {
     unsafe { CString::from_raw(p) };
 }
 
+#[no_mangle]
+pub extern "C" fn free_coordinates(p: *mut c_void) {
+    // println!("{:p} pointer", p);
+    unsafe { Box::from_raw(p) };
+}
+
 #[repr(C)]
 pub struct Coord {
     pub latitude: f64,
     pub longitude: f64
+}
+
+#[repr(C)]
+pub struct Coordinates {
+    pub data: *const c_void,
+    pub nmemb: usize
 }
 
 #[no_mangle]
@@ -39,38 +51,98 @@ pub extern "C" fn encode_coordinates_ffi(points: *mut Coord,
     CString::new(result).unwrap().into_raw()
 }
 
+fn coord_vec_to_c(res: Vec<[f64; 2]>, points: *mut Coord, npoints: *mut usize) -> c_int {
+    unsafe {
+        if res.len() > *npoints {
+            *npoints = res.len();
+            return -1;
+        }
+    }
+
+    let len = res.len();
+    let slice = unsafe {
+        slice::from_raw_parts_mut(points, *npoints)
+    };
+
+    for i in 0..len as usize {
+        let pair = res[i];
+        slice[i].latitude = pair[0];
+        slice[i].longitude = pair[1];
+    }
+
+    unsafe { *npoints = res.len(); }
+    0
+}
+
+#[no_mangle]
+pub extern "C" fn decode_polyline_ffi(points: *mut Coord,
+                                      npoints: *mut usize,
+                                      src: *const c_char) -> c_int {
+    let s: String = unsafe {
+        CStr::from_ptr(src).to_string_lossy().into_owned()
+    };
+
+    let res = decode_polyline(s, 8);
+
+    match res {
+        Ok(vec) => coord_vec_to_c(vec, points, npoints),
+        Err(_) => -1,
+    }
+}
+
+fn coord_vec_to_c2(coordinates: Vec<[f64; 2]>) -> Coordinates {
+    let len: usize = coordinates.len();
+
+    let data = coordinates.into_boxed_slice();
+    let raw = Box::into_raw(data);
+
+    // println!("{:p} pointer", raw);
+    let res: Coordinates = Coordinates{nmemb: len, data: raw as *const c_void};
+    res
+}
+
+#[no_mangle]
+pub extern "C" fn decode_polyline_ffi2(src: *const c_char) -> Coordinates {
+    let s: String = unsafe {
+        CStr::from_ptr(src).to_string_lossy().into_owned()
+    };
+
+    let res = decode_polyline(s, 5);
+
+    match res {
+        Ok(vec) => coord_vec_to_c2(vec),
+        Err(_) => Coordinates{nmemb: 0, data: ptr::null_mut()},
+    }
+}
+
 #[no_mangle]
 pub extern "C" fn encode_coordinates_ffi2(dst: *mut c_char,
                                           n: usize,
                                           points: *mut Coord,
                                           npoints: usize) -> c_int {
     let mut cs: Vec<[f64; 2]> = Vec::new();
-    unsafe {
-	    let slice = slice::from_raw_parts_mut(points, npoints);
+    let slice = unsafe { slice::from_raw_parts_mut(points, npoints) };
 
-	    for coord in slice {
-	        cs.push([coord.latitude, coord.longitude]);
-	    }
+	for coord in slice {
+	    cs.push([coord.latitude, coord.longitude]);
     };
 
-    let tuple = match encode_coordinates(&cs, 5) {
+    let (rc, result) = match encode_coordinates(&cs, 5) {
 	    Ok(res) => (0, res),
 	    Err(res) => (-1, res)
     };
-    let (rc, result) = tuple;
 
     if rc < 0 || result.len() >= n {
 	    return -1;
     }
 
     let mut i: usize = 0;
-    unsafe {
-	    let slice2 = slice::from_raw_parts_mut(dst, n);
-	    for c in result.chars() {
-	        slice2[i] = c as i8;
-	        i = i + 1;
-	    }
-	    slice2[i] = 0;
-    };
+    let slice2 = unsafe { slice::from_raw_parts_mut(dst, n) };
+
+	for c in result.chars() {
+	    slice2[i] = c as i8;
+	    i = i + 1;
+	}
+	slice2[i] = 0;
     0
 }
